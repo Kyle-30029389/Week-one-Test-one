7a9a725b3e893252e4fbed898abd653e9c5da1cd
kyle-30029389 <30029389@student.toiohomai.ac.nz>
2019-07-25 13:31:20 +1200

    Finsihed task two and three

---
+++ ConsoleApp1/Task Three/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number one");
+            int one = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter number two");
+            int two = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter decimal one");
+            double DecOne = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter decimal two");
+            double DecTwo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter String one");
+            string StrOne = Console.ReadLine();
+
+            Console.WriteLine("Please enter String two");
+            string StrTwo = Console.ReadLine();
+
+            Console.WriteLine($"Number One: {one}\nNumber Two: {two}\nDecimal One: {DecOne}\nDecimal Two: {DecTwo}\nString One: {StrOne}\nString Two: {StrTwo}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Task Two/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your Age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your Address?");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("What is your Email ?");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Please enter a 4 digit number");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}\nAge: {age}\nAdress: {address}\nEmail : {email}\nNumber: {number}");
+            Console.ReadLine();
+        }
+    }
+}
