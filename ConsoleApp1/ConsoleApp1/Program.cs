9971c6ecca274845386ab5472341cc4e0d9e23d8
kyle-30029389 <30029389@student.toiohomai.ac.nz>
2019-07-25 13:02:52 +1200

    Asks for name and displays name back

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + FirstName + " " + LastName);
+            Console.WriteLine("Your name is {0} {1}", FirstName, LastName);
+            Console.WriteLine($"Your name is {FirstName} {LastName}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
